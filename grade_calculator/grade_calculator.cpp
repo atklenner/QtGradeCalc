95a2e03532e2a2eb8a4026ee103b8975aee28173
Andrew <atklenner@gmail.com>
2019-03-02 15:57:00 -0800

    More updates made. Much better, actually works somewhat.

--- grade_calculator/grade_calculator.cpp
+++ grade_calculator/grade_calculator.cpp
@@ -6,9 +6,28 @@ grade_calculator::grade_calculator(QWidget *parent) :
     ui(new Ui::grade_calculator)
 {
     ui->setupUi(this);
+    QObject::connect(ui->pushButton,SIGNAL(clicked(bool)),
+                         this,SLOT(update_overall(bool)));
 }
 
 grade_calculator::~grade_calculator()
 {
     delete ui;
+}
+
+void grade_calculator::update_overall(bool unused){
+    double score = 0;
+if(ui->radioButton->isChecked()){
+    score = (ui->hw_1->value() + ui->hw_2->value()
+            + ui->hw_3->value() + ui->hw_4->value()
+            + ui->hw_5->value() + ui->hw_6->value()
+            + ui->hw_7->value() + ui->hw_8->value()) * 0.25
+            + (ui->midterm_1->value()) * 0.20 + (ui->midterm_2->value()) * 0.20
+            + (ui->final_2->value()) * 0.35;
+} else {
+    score = 10;
+}
+    ui->lcdNumber->display(QString::number(score));
+
+    return;
 }
