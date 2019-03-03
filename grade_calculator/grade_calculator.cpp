f9b6b9d43bd583943c01d33b9733e86a2604f8e3
Andrew <atklenner@gmail.com>
2019-03-03 14:50:40 -0800

    Completed project. Almost certainly forgot to do something.

--- grade_calculator/grade_calculator.cpp
+++ grade_calculator/grade_calculator.cpp
@@ -21,18 +21,18 @@ if(ui->radioButton->isChecked()){
     score = (ui->hw_1->value() + ui->hw_2->value()
             + ui->hw_3->value() + ui->hw_4->value()
             + ui->hw_5->value() + ui->hw_6->value()
-            + ui->hw_7->value() + ui->hw_8->value()) * 0.25
-            + (ui->midterm_1->value()) * 0.20 + (ui->midterm_2->value()) * 0.20
-            + (ui->final_2->value()) * 0.35;
+            + ui->hw_7->value() + ui->hw_8->value()) * (0.25/160)
+            + (ui->midterm_1->value()+ ui->midterm_2->value()) * (0.40/200)
+            + (ui->final_2->value()) * (0.35/100);
 } else {
     score = (ui->hw_1->value() + ui->hw_2->value()
             + ui->hw_3->value() + ui->hw_4->value()
             + ui->hw_5->value() + ui->hw_6->value()
-            + ui->hw_7->value() + ui->hw_8->value()) * 0.25
-            + (ui->midterm_1->value()) * 0.30
-            + (ui->final_2->value()) * 0.44;
+            + ui->hw_7->value() + ui->hw_8->value()) * (0.25/160)
+            + (ui->midterm_1->value()) * (0.30/100)
+            + (ui->final_2->value()) * (0.44/100);
 }
-    ui->lcdNumber->display(QString::number(score));
+    ui->lcdNumber->display(QString::number(score * 100));
 
     return;
 }
